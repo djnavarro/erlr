72daca7ec727f43cd485602b1b4cad412a917b77
Danielle Navarro <djnavarro@protonmail.com>
2026-03-28 19:36:19 +1100

    update data

--- R/data.R
+++ R/data.R
@@ -1,30 +1,22 @@
 
 .cut_exposure_quantile <- function(exposure, n = 4, is_placebo = NULL) {
-
   if (is.null(is_placebo)) is_placebo <- exposure == 0
-
   breaks <- tibble::tibble(exposure, is_placebo) |>
     dplyr::filter(!is_placebo) |>
     dplyr::pull(exposure) |>
     stats::quantile(probs = (0:n)/n, na.rm = TRUE)
-
   exp_bin <- as.numeric(dplyr::case_when(
     is_placebo ~ "0",
     is.na(exposure) ~ NA_character_,
     TRUE ~ cut(exposure, breaks, labels = 1:n, include.lowest = TRUE)
   ))
-
   exp_quantile <- exp_bin |>
-    factor(
-      levels = 0:n,
-      labels = c("Placebo", paste0("Q", 1:n))
-    )
-  
+    factor(levels = 0:n, labels = c("Placebo", paste0("Q", 1:n)))  
   return(exp_quantile)
 }
 
-.lr_data <- function(seed) {
-  set.seed(3434L)
+.make_lr_data <- function(seed) {
+  set.seed(seed)
   n <- 300L
   lr_dat <- tibble::tibble(
     id = 1:n,
@@ -43,7 +35,7 @@
   return(lr_dat)
 }
 
-# lr_dat <- .lr_data(seed = 2407L)
+# lr_dat <- .make_lr_data(seed = 2407L)
 # usethis::use_data(lr_dat, overwrite = TRUE)
 
 #' Sample simulated data for logistic regression exposure-response models with covariates.
