27feb08125376910b636c62e68fbbd019959a267
Danielle Navarro <djnavarro@protonmail.com>
2026-04-07 22:06:44 +1000

    adds build_model_spaghetti

--- R/lr-plot-partials-model.R
+++ R/lr-plot-partials-model.R
@@ -66,6 +66,90 @@ build_model_ribbonline <- function(data, config, stratify, exposure, response, s
 #' @export
 build_model_spaghetti <- function(data, config, stratify, exposure, response, strata, style) {
 
-  geoms <- NULL
+  fn <- lr_simulator(config$glm)
+
+  nsim <- 100L
+  seed <- config$seed
+  if (is.null(seed)) {
+    seed <- .pick_seed()
+    rlang::inform(paste("Using seed =", seed))
+  }
+  withr::with_seed(
+    seed = seed,
+    code = {
+      par <- mvtnorm::rmvnorm(
+        n = nsim, 
+        mean = stats::coef(config$glm),
+        sigma = stats::vcov(config$glm)
+      )
+    }
+  )
+
+  sim <- list()
+  for (ii in 1:nsim) {
+    dd_sim <- config$prediction |> 
+      dplyr::select(dplyr::all_of(c(exposure$name, "fit_resp", strata$name))) |> 
+      dplyr::mutate(row_id = dplyr::row_number(), sim_id = ii)
+    dd_sim[[response$name]] <- 1
+    dd_sim$fit_resp <- fn(param = par[ii,], dd_sim)
+    dd_sim[[response$name]] <- NULL
+    sim[[ii]] <- dd_sim
+  }
+  sim <- dplyr::bind_rows(sim)
+
+  if (stratify == FALSE) {
+
+    model_spaghetti <- ggplot2::geom_path(
+      data = sim,
+      mapping = ggplot2::aes(
+        x = .data[[exposure$name]],
+        y = .data[["fit_resp"]],
+        group = .data[["sim_id"]]
+      ),
+      fill = "grey40",
+      alpha = .1,
+      key_glyph = style$draw_key
+    )
+
+    model_line <- ggplot2::geom_path(
+      data = config$predictions,
+      mapping = ggplot2::aes(
+        x = .data[[exposure$name]], 
+        y = fit_resp
+      ),
+      linewidth = 1,
+      key_glyph = style$draw_key
+    )
+  }
+
+  if (stratify == TRUE) {
+
+    model_spaghetti <- ggplot2::geom_path(
+      data = sim |> 
+        dplyr::mutate(sim_id2 = paste(.data[["sim_id"]], .data[[strata$name]])),
+      mapping = ggplot2::aes(
+        x = .data[[exposure$name]],
+        y = .data[["fit_resp"]],
+        color = .data[[strata$name]],
+        group = .data[["sim_id2"]]
+      ),
+      fill = "grey40",
+      alpha = .25,
+      key_glyph = style$draw_key
+    )
+
+    model_line <- ggplot2::geom_path(
+      data = config$predictions,
+      mapping = ggplot2::aes(
+        x = .data[[exposure$name]], 
+        y = fit_resp,
+        color = .data[[strata$name]]
+      ),
+      linewidth = 1,
+      key_glyph = style$draw_key
+    )    
+  }
+  
+  geoms <- list(model_spaghetti, model_line)
   return(geoms)
 }
--- tests/testthat/test-lr-plot-partials-model.R
+++ tests/testthat/test-lr-plot-partials-model.R
@@ -1,5 +1,5 @@
 
-test_that("build_model_ribbonline returns geom + coord", {
+test_that("build_model_ribbonline returns 2 geoms", {
   p1 <- lr_plot(lr_data, aucss, ae1)
   p2 <- lr_plot(lr_data, aucss, ae1, sex)
 
@@ -42,6 +42,69 @@ test_that("build_model_ribbonline returns geom + coord", {
       style = p1$style
     )
   p2_out <- build_model_ribbonline(
+      data = p2$data,
+      config = p2$part$model$config,
+      stratify = p2$part$model$stratify,
+      exposure = p2$exposure,
+      response = p2$response,
+      strata = p2$strata,
+      style = p2$style
+    )
+
+  expect_length(p1_out, 2)
+  expect_length(p2_out, 2)
+
+  expect_true(inherits(p1_out[[1]], "LayerInstance"))
+  expect_true(inherits(p1_out[[2]], "LayerInstance"))
+
+  expect_true(inherits(p2_out[[1]], "LayerInstance"))
+  expect_true(inherits(p2_out[[2]], "LayerInstance"))
+})
+
+
+test_that("build_model_spaghetti returns 2 geoms", {
+  p1 <- lr_plot(lr_data, aucss, ae1)
+  p2 <- lr_plot(lr_data, aucss, ae1, sex)
+
+  expect_no_error(p1 |> lr_plot_show_model(style = "spaghetti"))
+  expect_no_error(p2 |> lr_plot_show_model(style = "spaghetti"))
+
+  p1 <- p1 |> lr_plot_show_model(style = "spaghetti")
+  p2 <- p2 |> lr_plot_show_model(style = "spaghetti")
+
+  expect_no_error(
+    build_model_spaghetti(
+      data = p1$data,
+      config = p1$part$model$config,
+      stratify = p1$part$model$stratify,
+      exposure = p1$exposure,
+      response = p1$response,
+      strata = p1$strata,
+      style = p1$style
+    )
+  )
+  expect_no_error(
+    build_model_spaghetti(
+      data = p2$data,
+      config = p2$part$model$config,
+      stratify = p2$part$model$stratify,
+      exposure = p2$exposure,
+      response = p2$response,
+      strata = p2$strata,
+      style = p2$style
+    )
+  )
+
+  p1_out <- build_model_spaghetti(
+      data = p1$data,
+      config = p1$part$model$config,
+      stratify = p1$part$model$stratify,
+      exposure = p1$exposure,
+      response = p1$response,
+      strata = p1$strata,
+      style = p1$style
+    )
+  p2_out <- build_model_spaghetti(
       data = p2$data,
       config = p2$part$model$config,
       stratify = p2$part$model$stratify,
